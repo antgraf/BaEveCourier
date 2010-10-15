8299ee6bd5570fba6a0a2e66371b20a30f2ae85a
antgraf <antgraf@gmail.com>
2010-10-15 22:12:37 +0400

    Plugin framework mandatory classes declared.

---
+++ Courier/CourierActor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExecutionActors;
+
+namespace Courier
+{
+	class CourierActor : Actor
+	{
+		protected override void Init(object data)
+		{
+			pObserver.Notify(this, "Initialization", 100, "Eve Courier actor initialized");
+		}
+
+		protected override void Worker()
+		{
+			pObserver.Notify(this, "Start", 100, "Eve Courier actor started");
+			pObserver.Notify(this, "TODO", 0, "NOT IMPLEMENTED");
+			pObserver.Notify(this, "End", 100, "Eve Courier actor finished");
+		}
+	}
+}
---
+++ Courier/CourierPlugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExecutionActors;
+using System.Windows.Forms;
+
+namespace Courier
+{
+	class CourierPlugin : PluginBase
+	{
+		public CourierPlugin()
+		{
+			pPluginName = "Eve Courier BA plug-in";
+			pActor = ActorsMan.NewActor(typeof(CourierActor), this);
+		}
+
+		public override void ShowUI()
+		{
+			if(MessageBox.Show("NOT IMPLEMENTED\r\nStart worker?", "TODO", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+				== DialogResult.Yes)
+			{
+				Run();
+			}
+		}
+	}
+}
